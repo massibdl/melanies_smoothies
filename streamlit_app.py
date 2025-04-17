2cda5176ea52d548fbcbe3959ddea2dc7aa5f041
massibdl <84510084+massibdl@users.noreply.github.com>
2025-04-17 17:27:41 +0200

    Update streamlit_app.py
    fix the not found error

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,10 +33,10 @@ if ingredients_list:
     for i in ingredients_list:
         ingredients_string += i + ' '
         search_on=pd_df.loc[pd_df['FRUIT_NAME'] == i, 'SEARCH_ON'].iloc[0]
-        st.write('The search value for ', i,' is ', search_on, '.')
+        #st.write('The search value for ', i,' is ', search_on, '.')
       
         st.subheader(i + ' Nutrition Information')
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ i)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/"+ SEARCH_ON)
         st_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
     #st.write(ingredients_string)
     
